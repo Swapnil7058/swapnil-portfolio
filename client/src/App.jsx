89eef5492e621355945ce2d91e0e23da43980600
Swapnil7058 <swapnildhotre9767@gmail.com>
2026-06-04 17:50:16 +0530

    Create navbar and hero section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 function App() {
-
   return (
     <>
-      <h1>Swapnil Dhotre</h1>
-      <h2>MERN Stack Developer</h2>
-
-      <p>
-        React.js | Node.js | Express.js | MongoDb
-      </p>
+      <Navbar />
+      <Hero />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
