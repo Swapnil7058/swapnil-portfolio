e1bd07c76f0715764d2020e6f6d6ed931de29726
Swapnil7058 <swapnildhotre9767@gmail.com>
2026-06-05 16:08:14 +0530

    Setup Tailwind CSS, React Icons and Framer Motion

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
-import "./App.css";
+// import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 
 function App() {
   return (
     <>
+    <h1 className="text-5xl font-bold text-center text-amber-500 mt-10">Test</h1>
       <Navbar />
       <Hero />
     </>
