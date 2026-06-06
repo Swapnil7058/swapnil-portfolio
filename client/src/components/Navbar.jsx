41be00e1c5629ffad9efc81da7a79357042168f5
Swapnil7058 <swapnildhotre9767@gmail.com>
2026-06-06 16:28:43 +0530

    feat(navbar): add sticky glass navigation

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,17 +2,51 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav>
-      <h2>Swapnil Dhotre</h2>
+    <header
+      className="
+    fixed
+    top-0
+    left-0
+    w-full
+    z-50
+    background-blur-md
+    bg-slate-900/70
+    border-b
+    border-slate-800
+    "
+    >
+      <div className=" container-custom mx-auto px-6">
+        <div className="flex items-center justify-between h-20">
+          {/* Logo */}
 
-      <ul>
-        <li>Home</li>
-        <li>Skills</li>
-        <li>Projects</li>
-        <li>Experience</li>
-        <li>Contact</li>
-      </ul>
-    </nav>
+          <a href="#home" className="text-2xl font-bold text-cyan-400">
+            My Portfolio
+          </a>
+
+          <nav className="hidden md:flex gap-8">
+            <a href="#home" className="hover:text-cyan-400 transition">
+              Home
+            </a>
+
+            <a href="#about" className="hover:text-cyan-400 transition">
+              About
+            </a>
+
+            <a href="#skills" className="hover:text-cyan-400 transition">
+              Skills
+            </a>
+
+            <a href="#projects" className="hover:text-cyan-400 transition">
+              Projects
+            </a>
+
+            <a href="#contact" className="hover:text-cyan-400 transition">
+              Contact
+            </a>
+          </nav>
+        </div>
+      </div>
+    </header>
   );
 };
 
